977ed5c1d24e1678788458c920821e21bc1381e9
Abstractn <>
2024-12-11 10:53:57 +0100

    Installed Vite Handlebars plugin and added partials folder configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,10 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import handlebars from 'vite-plugin-handlebars';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default {
   base: '/abs-webtools/dist/',
   build: {
@@ -5,5 +12,10 @@ export default {
     terserOptions: {
       keep_classnames: true,
     },
-  }
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: path.resolve(__dirname, 'src'),
+    }),
+  ],
 }
