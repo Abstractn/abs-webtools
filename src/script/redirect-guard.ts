70a759e83c6dc86b065ca86064019e24efd4ac4c
Abstractn <>
2025-04-18 14:31:16 +0200

    Generated build

--- src/script/redirect-guard.ts
+++
@@ -1,8 +0,0 @@
-(() => {
-  const originPathName = '/abs-webtools/';
-  const targetPathName = '/abs-webtools/dist/';
-  const currentPathName = window.location.pathname;
-  if(currentPathName === originPathName) {
-    window.location.href = targetPathName;
-  }
-})();
