795e6caabe623437d79b20d2dcc8918721afaba7
Abstractn <>
2025-12-11 15:01:20 +0100

    Added GitHub automated deploy pipeline script and updated website base url references

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  base: '/abs-webtools/dist/',
+  base: '/abs-webtools/',
   build: {
     minify: 'terser',
     terserOptions: {
       keep_classnames: true,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        logger: {
+          warn: function(message: string, options: { deprecation: boolean }) {
+            if(options.deprecation) return;
+            console.warn(message);
+          }
+        }
+      },
     },
   },
   plugins: [
