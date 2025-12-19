5609e117d6eb4a3c5e868e9116f50513ef0f3144
Abstractn <>
2025-12-19 09:18:16 +0100

    Removed unused code and comments

--- src/components/page/page.component.ts
+++ src/components/page/page.component.ts
@@ -1,13 +1,7 @@
 import { AbsComponent } from 'abs-component';
 
 export class Page implements AbsComponent {
-  constructor(public readonly node: HTMLElement) {
-    this.aniCalcViewNode = this.node.getNode('view#ani-calc') as HTMLElement;
-    //TODO
-  }
-
-  public readonly aniCalcViewNode: HTMLElement;
-  //TODO
+  constructor(public readonly node: HTMLElement) {}
 
   init() {}
 
